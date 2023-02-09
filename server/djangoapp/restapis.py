b40c299985b59a88a6e496caa9631b455a5537a4
kevinmbuicode <kevinmushan@gmail.com>
2023-02-09 21:28:27 +0300

    feat: create python rest apis

---
+++ server/djangoapp/restapis.py
@@ -0,0 +1,111 @@
+from .models import CarDealer
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+from django.shortcuts import params, api_key, request
+
+
+# Create a  method to get reviews by dealer id from a cloud function
+# - Call get_request() with specified arguments
+# - Parse JSON results into a DealerView object list
+def get_dealers_by_id(url, dealerId):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["rows"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
+
+
+# Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
+# :
+# - Call get_request() with specified arguments
+# - Get the returned sentiment label such as Positive or Negative
+
+
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
+
+
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    # Call get_request with a URL parameter
+    json_result = get_request(url)
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["rows"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+            dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
+
+
+def get_dealer_by_id_from_cf(url, dealerId):
+    results = []
+
+    json_result = get_request(url)
+    if json_result:
+        dealers = json_result["rows"]
+        for dealer in dealers:
+            dealer_doc = dealer["doc"]
+            dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+
+    return results
+
+
+def get_dealer_reviews_from_cf(url, dealerId):
+    requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                 auth=HTTPBasicAuth('apikey', api_key))
+    if api_key:
+        # Basic authentication GET
+        request.get(url, params=params, auth=...)
+    else:
+        # no authentication GET
+        request.get(url, params=params)
+
+
+def analyze_review_sentiments(text):
+    return
+    params = dict()
+    params["text"] = kwargs["text"]
+    params["version"] = kwargs["version"]
+    params["features"] = kwargs["features"]
+    params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+    response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                            auth=HTTPBasicAuth('apikey', api_key))
